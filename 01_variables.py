cade9b56ecf4c5346e33e4fc5cb82658209e464c
manikanta Routhu <manikantar129@gmail.com>
2026-06-24 12:17:53 +0530

    python scripting

--- 01_variables.py
+++ 01_variables.py
@@ -3,7 +3,22 @@
 This is a multi-line comment
 """
 # Define a variable
-a = 42
+a = 42 # integer
 
 # Print the value that is stored inside the variable
 print(a)
+
+# Float
+b = 42.32
+print(b)
+
+# Boolean
+c = True 
+
+# string
+d = 'this is string'
+e = "this is also a string"
+f = """
+This is a mutli line string
+"""
+print(d)
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # python-scripting
+variables
+------------
+- it is container to store values inside
+python scripts are saved using .py
+
+- integer
+- Boolen
+- String
+- Float
+
+python have saparate varibles
+
+- List
+- Tuple
+- Set
+- Dictionary
+
+python is case-sensitive i.e devops and DevOps 
