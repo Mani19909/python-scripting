4dde3f5c2b9a89f94f86a329314980aee1b167e2
manikanta Routhu <manikantar129@gmail.com>
2026-06-24 16:07:48 +0530

    python scripting

--- 01_variables.py
+++ 01_variables.py
@@ -56,7 +56,8 @@ print(res, res_1, res_2, res_3)
 a = True
 b = False
 res = a and b
-res_1 = a not b
+res_1 =  not b
+res_3 =  not a
 res_2 = a or b
 
-print(res, res_1, res_2)
+print(res, res_1, res_2, res_3)
