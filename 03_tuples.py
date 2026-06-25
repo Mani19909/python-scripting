26a3f26cb24188df3f68b57d657ebfd3c5570359
manikanta Routhu <manikantar129@gmail.com>
2026-06-25 11:06:18 +0530

    python scripting

--- 03_tuples.py
+++ 03_tuples.py
@@ -1,6 +1,16 @@
 # There are 2 methods to create a tuple
 # 1. ()
 # 2. tuple()
-# it behavious like immutable
+# Behaviour: They're immutable
 
-sample_tuple =
+sample_tuple = ("Ansible", "Terraform", "Jenkins", "Docker", "K8s") # Tool set
+
+sample_ele = sample_tuple[1]
+print(sample_ele)
+
+sample_ele = sample_tuple[-1]
+print(sample_ele)
+
+# Slicing
+sliced_tuple = sample_tuple[1:3]
+print(sliced_tuple)
